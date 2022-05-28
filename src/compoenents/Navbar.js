9914bd56847eb9c99d3521b564d2f18d427209e3
karan919 <karanmahajan919@gmail.com>
2022-05-28 17:10:01 +0530

    Added navbar

---
+++ src/compoenents/Navbar.js
@@ -0,0 +1,38 @@
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import RssFeedIcon from "@mui/icons-material/RssFeed";
+import { useNavigate } from "react-router-dom";
+
+const Navbar = () => {
+  let navigate = useNavigate();
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <RssFeedIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            onClick={() => navigate("/")}
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            PostFeed
+          </Typography>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+
+export default Navbar;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,12 @@
+import Navbar from "../compoenents/Navbar";
+
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <Navbar />
+      <h1>Home</h1>
+    </>
+  );
 };
 
 export default Home;
